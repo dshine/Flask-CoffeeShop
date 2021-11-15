5931795d82d81cb9b4266dd45fc1c9f6cf611e44
Don Shine <dshine@gofree.indigo.ie>
2021-11-15 21:03:12 +0100

    added requests

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 from app import app
+
+import requests, json
 
 @app.route("/")
 def home():
@@ -15,4 +16,18 @@ def about():
 @app.route("/menu")
 def menu():
     title = "Tasty Treats"
-    return render_template('about.html', title=title)
+    url = "https://opensheet.vercel.app/17B5XfOn0S4LSFWUFmRMZvpodoqf2lDoJAyO4ZFnNieg/sheet1"
+    response = requests.request("GET", url)
+    data = response.json()
+    return render_template('about.html', title=title, coffees=data)
+
+@app.route("/randos")
+def randomusers():
+    url = "https://randomuser.me/api/?results=50"
+    response = requests.request("GET", url)
+    # print(response.status_code)
+    # print(response.headers)
+    # print(response.text)
+    # print(response.json())
+    data = response.json()
+    return render_template('random.html', persons=data['results'])
