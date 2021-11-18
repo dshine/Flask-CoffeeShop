1ab7ca312b3801b6348ceba3993783aee8ed7b0f
Don Shine <dshine@gofree.indigo.ie>
2021-11-18 23:39:34 +0100

    Added dynamic jumbotron to each page

--- app/views.py
+++ app/views.py
@@ -6,23 +6,39 @@ import requests, json
 @app.route("/")
 def home():
     title = "Home Page"
-    return render_template('index.html', title=title)
+    jumbotron = {
+        "title": "My Cool Title",
+        "body": "Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking."
+    }
+    return render_template('index.html', title=title, content=jumbotron)
 
 @app.route("/about")
 def about():
     title = "About Page"
-    return render_template('about.html', title=title) 
+    jumbotron = {
+        "title": "About Us",
+        "body": "You can add a quick description to this section that will highlight some key features"
+    }
+    return render_template('about.html', title=title, content=jumbotron) 
 
 @app.route("/menu")
 def menu():
     title = "Tasty Treats"
+    jumbotron = {
+        "title": "Yummmmmmyyy!!!",
+        "body": "Check out our selection of rich coffees, exotic teas and delicious treats"
+    }
     url = "https://opensheet.vercel.app/17B5XfOn0S4LSFWUFmRMZvpodoqf2lDoJAyO4ZFnNieg/sheet1"
     response = requests.request("GET", url)
     data = response.json()
-    return render_template('about.html', title=title, coffees=data)
+    return render_template('about.html', title=title, coffees=data, content=jumbotron)
 
 @app.route("/randos")
 def randomusers():
+    jumbotron = {
+        "title": "Our Staff",
+        "body": "It's not easy having a good time! Even smiling makes my face ache!"
+    }
     url = "https://randomuser.me/api/?results=50"
     response = requests.request("GET", url)
     # print(response.status_code)
@@ -30,4 +46,4 @@ def randomusers():
     # print(response.text)
     # print(response.json())
     data = response.json()
-    return render_template('random.html', persons=data['results'])
+    return render_template('random.html', persons=data['results'], content=jumbotron)
